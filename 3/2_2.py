a69dc3291750325a466185c470cf7a413c730f88
ShneidmanIAT <91080676+ShneidmanIAT@users.noreply.github.com>
2021-10-11 23:31:21 +0300

    refactored code of 2_2
    remade functions of drawing and refactored code, added some documenting

--- 3/2_2.py
+++ 3/2_2.py
@@ -1,85 +1,129 @@
 import pygame
 import math as m
-from pygame.draw import *
 
-pygame.init()
 
 FPS = 30
-screen_size=800
-screen=pygame.display.set_mode((screen_size,screen_size))
-#небо+земля
-rect(screen,'cyan', (0,0,screen_size,screen_size))
-rect(screen,(0,200,0), (0,screen_size/2,screen_size,screen_size/2))
-#дом
-house=pygame.Surface((100,100))
-house=pygame.Surface.convert_alpha(house)
-house.fill((0,0,0,0))
-rect(house,(200,100,0),(0,40,100,60))
-rect(house,(0,0,0),(0,40,100,60),1)
-polygon(house,'red',[(50,0),(100,40),(0,40)])
-polygon(house,'black',[(50,0),(100,40),(0,40)],1)
-rect(house,(80,80,250),(35,55,30,30))
-rect(house,(0,0,0),(35,55,30,30),1)
-#облако
-cloud=pygame.Surface((100,60))
-cloud=pygame.Surface.convert_alpha(cloud)
-cloud.fill((0,0,0,0))
-circle(cloud,'white',(40,20),20)
-circle(cloud,'black',(40,20),20,1)
-circle(cloud,'white',(60,20),20)
-circle(cloud,'black',(60,20),20,1)
-circle(cloud,'white',(20,40),20)
-circle(cloud,'black',(20,40),20,1)
-circle(cloud,'white',(40,40),20)
-circle(cloud,'black',(40,40),20,1)
-circle(cloud,'white',(60,40),20)
-circle(cloud,'black',(60,40),20,1)
-circle(cloud,'white',(80,40),20)
-circle(cloud,'black',(80,40),20,1)
-#дерево
-tree=pygame.Surface((60,100))
-tree=pygame.Surface.convert_alpha(tree)
-tree.fill((0,0,0,0))
-rect(tree,(100,50,0),(25,50,10,50))
-circle(tree,(0,100,0),(30,20),20)
-circle(tree,(0,0,0),(30,20),20,1)
-circle(tree,(0,100,0),(20,30),20)
-circle(tree,(0,0,0),(20,30),20,1)
-circle(tree,(0,100,0),(40,30),20)
-circle(tree,(0,0,0),(40,30),20,1)
-circle(tree,(0,100,0),(30,40),20)
-circle(tree,(0,0,0),(30,40),20,1)
-circle(tree,(0,100,0),(20,50),20)
-circle(tree,(0,0,0),(20,50),20,1)
-circle(tree,(0,100,0),(40,50),20)
-circle(tree,(0,0,0),(40,50),20,1)
-#солнце
-sun=pygame.Surface((100,100))
-sun=pygame.Surface.convert_alpha(sun)
-sun.fill((0,0,0,0))
-r1=50
-r2=45
-alpha=0
-number_of_rays=30
-sunrays=[(0,0)]*number_of_rays
-for i in range (0,number_of_rays):
-    if i%2==0:
-        sunrays[i]=(r1*m.cos(alpha)+50,r1*m.sin(alpha)+50)
-    else:
-        sunrays[i]=(r2*m.cos(alpha)+50,r2*m.sin(alpha)+50)
-    alpha+=2*m.pi/number_of_rays
-polygon(sun,'yellow',sunrays)
-polygon(sun,'black',sunrays,1)
-#отрисовка обьектов в формате screen.blit(pygame.transform.scale(название обьекта,(х размер,у размер)),(у координата,х координата))
-screen.blit(pygame.transform.smoothscale(house,(300,300)),(10,300))
-screen.blit(pygame.transform.smoothscale(tree,(216,300)),(320,280))
-screen.blit(pygame.transform.smoothscale(house,(150,150)),(540,350))
-screen.blit(pygame.transform.smoothscale(tree,(108,150)),(690,330))
-screen.blit(pygame.transform.smoothscale(cloud,(200,120)),(550,100))
-screen.blit(pygame.transform.smoothscale(cloud,(100,60)),(350,150))
-screen.blit(pygame.transform.smoothscale(cloud,(150,90)),(140,100))
-screen.blit(pygame.transform.smoothscale(sun,(100,100)),(50,50))
-#события
+SCREENSIZE = 800
+GRASSGREEN = (0, 200, 0)
+BLACK = (0, 0, 0)
+TREEGREEN = (0, 100, 0)
+BROWN = (100, 50, 0)
+HOUSEBROWN = (200, 100, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+HOUSEBLUE = (80, 80, 250)
+BLUE = (100, 100, 250)
+YELLOW = (255, 255, 0)
+
+
+screen = pygame.display.set_mode((SCREENSIZE, SCREENSIZE))
+pygame.init()
+pygame.draw.rect(screen, BLUE, (0, 0, SCREENSIZE, SCREENSIZE))
+pygame.draw.rect(screen, GRASSGREEN, (0, SCREENSIZE / 2, SCREENSIZE, SCREENSIZE / 2))
+
+
+def draw_house(xpos, ypos, xsize, ysize):
+    """Draws a house on the display
+
+    xpos, ypos - координаты левого верхнего угла изображения
+    xsize, ysize - ширина и высота изобажения
+    """
+    house = pygame.Surface((100, 100))
+    house = pygame.Surface.convert_alpha(house)
+    house.fill((0, 0, 0, 0))
+    pygame.draw.rect(house, HOUSEBROWN, (0, 40, 100, 60))
+    pygame.draw.rect(house, BLACK, (0, 40, 100, 60), 1)
+    pygame.draw.polygon(house, RED, [(50, 0), (100, 40), (0, 40)])
+    pygame.draw.polygon(house, BLACK, [(50, 0), (100, 40), (0, 40)], 1)
+    pygame.draw.rect(house, HOUSEBLUE, (35, 55, 30, 30))
+    pygame.draw.rect(house, BLACK, (35, 55, 30, 30), 1)
+    screen.blit(pygame.transform.smoothscale(house, (xsize, ysize)), (xpos, ypos))
+
+
+def draw_cloud(xpos, ypos, xsize, ysize):
+    """Draws a cloud made of several circles on the display
+
+    xpos, ypos - координаты левого верхнего угла изображения
+    xsize, ysize - ширина и высота изобажения
+    """
+    cloud = pygame.Surface((1000, 600))
+    cloud = pygame.Surface.convert_alpha(cloud)
+    cloud.fill((0, 0, 0, 0))
+    pygame.draw.circle(cloud, WHITE, (400, 200), 200)
+    pygame.draw.circle(cloud, BLACK, (400, 200), 200, 10)
+    pygame.draw.circle(cloud, WHITE, (600, 200), 200)
+    pygame.draw.circle(cloud, BLACK, (600, 200), 200, 10)
+    pygame.draw.circle(cloud, WHITE, (200, 400), 200)
+    pygame.draw.circle(cloud, BLACK, (200, 400), 200, 10)
+    pygame.draw.circle(cloud, WHITE, (400, 400), 200)
+    pygame.draw.circle(cloud, BLACK, (400, 400), 200, 10)
+    pygame.draw.circle(cloud, WHITE, (600, 400), 200)
+    pygame.draw.circle(cloud, BLACK, (600, 400), 200, 10)
+    pygame.draw.circle(cloud, WHITE, (800, 400), 200)
+    pygame.draw.circle(cloud, BLACK, (800, 400), 200, 10)
+    screen.blit(pygame.transform.smoothscale(cloud, (xsize, ysize)), (xpos, ypos))
+
+
+def draw_tree(xpos, ypos, xsize, ysize):
+    """Draws a tree on the display
+
+    xpos, ypos - координаты левого верхнего угла изображения
+    xsize, ysize - ширина и высота изобажения
+    """
+    tree = pygame.Surface((600, 1000))
+    tree = pygame.Surface.convert_alpha(tree)
+    tree.fill((0, 0, 0, 0))
+    pygame.draw.rect(tree, BROWN, (250, 500, 100, 500))
+    pygame.draw.circle(tree, TREEGREEN, (300, 200), 200)
+    pygame.draw.circle(tree, BLACK, (30, 20), 200, 10)
+    pygame.draw.circle(tree, TREEGREEN, (200, 300), 200)
+    pygame.draw.circle(tree, BLACK, (200, 300), 200, 10)
+    pygame.draw.circle(tree, TREEGREEN, (400, 300), 200)
+    pygame.draw.circle(tree, BLACK, (400, 300), 200, 10)
+    pygame.draw.circle(tree, TREEGREEN, (300, 400), 200)
+    pygame.draw.circle(tree, BLACK, (300, 400), 200, 10)
+    pygame.draw.circle(tree, TREEGREEN, (200, 500), 200)
+    pygame.draw.circle(tree, BLACK, (200, 500), 200, 10)
+    pygame.draw.circle(tree, TREEGREEN, (400, 500), 200)
+    pygame.draw.circle(tree, BLACK, (400, 500), 200, 10)
+    screen.blit(pygame.transform.smoothscale(tree, (xsize, ysize)), (xpos, ypos))
+
+
+def draw_sun(xpos, ypos, xsize, ysize, number_of_rays):
+    """Draws a sun on the display
+
+    xpos, ypos - координаты левого верхнего угла изображения
+    xsize, ysize - ширина и высота изобажения
+    sunrays - количество лучей
+    """
+    sun = pygame.Surface((100, 100))
+    sun = pygame.Surface.convert_alpha(sun)
+    sun.fill((0, 0, 0, 0))
+    outerradius = 50
+    innerradius = 30
+    alpha = 100
+    sunrays = [(0, 0)] * number_of_rays
+    for i in range(0, number_of_rays):
+        if i % 2 == 0:
+            sunrays[i] = (innerradius * m.cos(alpha) + 50, innerradius * m.sin(alpha) + 50)
+        else:
+            sunrays[i] = (outerradius * m.cos(alpha) + 50, outerradius * m.sin(alpha) + 50)
+        alpha += 2 * m.pi / number_of_rays
+    pygame.draw.polygon(sun, YELLOW, sunrays)
+    pygame.draw.polygon(sun, BLACK, sunrays, 1)
+    screen.blit(pygame.transform.rotate(pygame.transform.smoothscale(sun, (xsize, ysize)), 30), (xpos, ypos))
+
+
+draw_house(300, 300, 10, 300)
+draw_tree(216, 300, 320, 280)
+draw_house(150, 150, 540, 350)
+draw_tree(108, 150, 690, 330)
+draw_cloud(200, 120, 550, 100)
+draw_cloud(100, 60, 350, 150)
+draw_cloud(150, 90, 140, 100)
+draw_sun(100, 100, 50, 50, 12)
+
+
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
